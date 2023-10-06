1869dea1e1192458597d2fcf077d82df810bb67e
dlibenp <dannerliben@gmail.com>
2023-10-06 07:45:05 -0400

    add uuid to id

--- solution/models/order.py
+++ solution/models/order.py
@@ -1,6 +1,7 @@
 from pydantic import BaseModel, Field
 from typing import Optional
 from enum import Enum
+from uuid import uuid4
 
 class Status(str, Enum):
     completed = "completed"
@@ -14,8 +15,8 @@ class Criterion(str, Enum):
     all = "all"
 
 class Order(BaseModel):
-    id: Optional[int] = None
+    id: Optional[str] = Field(default=uuid4())
     item: str = Field(default="New Order", min_length=3, max_length=255)
     quantity: int = Field(default=0, gt=0)
     price: float = Field(default=0, ge=0, le=1000000)
-    status: Status = None
+    status: Status = Field(default=Status.completed)
