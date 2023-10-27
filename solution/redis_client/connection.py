1ae717296c5cc866fb1c0edce3a69bd13579531c
Danner Liben <75746135+dlibenp@users.noreply.github.com>
2023-10-27 01:04:31 -0400

    Update connection.py
    Config redis url

--- solution/redis_client/connection.py
+++ solution/redis_client/connection.py
@@ -1,16 +1,19 @@
-from redis import Redis
+import redis
+#from redis import Redis
 from redis.exceptions import ConnectionError
-from os import getenv
+#from os import getenv
+import os
 import logging
 
 try:
-    redis_client = Redis(
-        host=getenv("REDIS_HOST"),
-        port=getenv("REDIS_PORT"),
-        password=getenv("REDIS_PASSWORD"),
-        db=getenv("REDIS_DB"),
-        ssl=bool(getenv("REDIS_SSL"))
-    )
+    #redis_client = Redis(
+    #    host=getenv("REDIS_HOST"),
+    #    port=getenv("REDIS_PORT"),
+    #    password=getenv("REDIS_PASSWORD"),
+    #    db=getenv("REDIS_DB"),
+    #    ssl=bool(getenv("REDIS_SSL"))
+    #)
+    redis_client = redis.from_url(os.environ['REDIS_URL'])
     logging.info("CONNECTED TO REDIS!!")
 except ConnectionError as cx:
     logging.error(f"CONNECTION ERROR: {cx}")
